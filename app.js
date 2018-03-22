0b6190b29cfc4c40319aaec923c708b559ebfb1a
Kohrak <gabrielgt95@gmail.com>
2018-03-22 21:07:08 +0000

    express app setup

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ var express = require("express"),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
     app = express();
+
+mongoose.connect("mongodb://localhost/men_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
