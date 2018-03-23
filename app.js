29b910f555d71144b0f5ce3d6468a87a9d804e5b
Kohrak <kohrakgk0h@gmail.com>
2018-03-23 12:43:04 +0000

    add create and new routes

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ app.get("/", function(req, res){
   res.redirect("/blogs")
 })
 
+//Index
+
 app.get("/blogs", function(req, res){
   Blog.find({}, function(err, blogs){
     if (err){
@@ -34,7 +36,34 @@ app.get("/blogs", function(req, res){
     }
   })
 })
+//New
 
-app.listen(3000, function(){
+app.get("/blogs/new", function(req, res){
+  res.render("new.ejs");
+})
+
+//Create
+app.post("/blogs", function(req, res){
+  Blog.create(req.body.blog, function(err, newBlog){
+    if (err){
+      console.log("Error creating new blog")
+      res.render("new.ejs")
+    } else {
+      console.log(newBlog)
+      res.redirect("/blogs");
+    }
+  })
+})
+
+//Local
+
+// app.listen(3000, function(){
+//     console.log("Server Started");
+// })
+
+//Cloud 9
+
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server Started");
 })
+
